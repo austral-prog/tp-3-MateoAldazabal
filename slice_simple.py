c8beb757f4c16ed44fc8c4797961c0eab112a6d5
MateoAldazabal <maldazabal@mail.austral.edu.ar>
2025-04-07 17:49:40 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -5,10 +5,8 @@ def slice_simple():
     Texto = "awesome"
     texto = Texto.lower()
     mitad = int(len(texto)/2)
-
     print(texto[0:3])
     print(texto[ mitad-1: mitad + 2])
     print(f'{texto[0:4]}{texto[-3:]}')
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
