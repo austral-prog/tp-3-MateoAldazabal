63d005f35f0a4c58296b026aa2a1d1bcef322c46
MateoAldazabal <maldazabal@mail.austral.edu.ar>
2025-04-07 22:39:17 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,8 +1,8 @@
 def slice_advanced():
     texto = input()
     
-    print(f'{texto[4::2]} \n')
+    print(f'{texto[4::2]}\n')
      
     texto2 = input()
-    print(f' {texto2[4::2]}')
+    print(f'{texto2[4::2]}')
 `
