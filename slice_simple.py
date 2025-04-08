e2d8a493fd7c52016d19672d76f3263101cfa006
MateoAldazabal <maldazabal@mail.austral.edu.ar>
2025-04-08 10:02:46 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,7 @@
 def slice_simple():
     texto = "Awesome"
     texto = texto.lower()
-    mitad = int(len(Texto)/2)
+    mitad = int(len(texto)/2)
     print(texto[0:3])
     print(texto[ mitad-1: mitad + 2])
     print(f'{texto[0:4]}{texto[-3:]}')
